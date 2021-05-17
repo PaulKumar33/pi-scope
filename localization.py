5a503556c0ea7a50264ad1d151dbd2540a38fe2e
Paulkumar33 <paul.kumar@mail.utoronto.ca>
2021-05-16 20:48:32 -0400

    have added multiple plots

--- localization.py
+++ localization.py
@@ -4,18 +4,49 @@ from PyQt5 import QtWidgets
 import sys
 from pyqtgraph.Qt import QtGui, QtCore
 from random import randint
+import os
+
+"""import mcp_3008_driver as mcp
+import RPi.GPIO as GPIO"""
 
 class Plot2D(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(Plot2D, self).__init__(*args, **kwargs)
 
-        self.plot = pg.PlotWidget()
-        self.setCentralWidget(self.plot)
+        """self.plot = pg.PlotWidget()
+        self.setCentralWidget(self.plot)"""
+
+        w = pg.GraphicsLayoutWidget()
+
+        #for signal 1
+        self.plot = w.addPlot(row=0, col=0)
+
+        #for signal 2
+        self.plot2 = w.addPlot(row=1, col=0)
+
+        #for the std1
+        self.plot3 = w.addPlot(row=2, col=0)
+        #for std2
+        self.plot4 = w.addPlot(row=2, col=1)
+        #for acculated
+        self.plot5 = w.addPlot(row=3, col=0)
+
+        self.setCentralWidget(w)
 
         """hour = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         temperature = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]"""
         self.x = np.arange(0,5.01, 0.01)
         self.y = [randint(-10,10) for _ in range(len(self.x))]
+
+        """if(os.name == 'posix'):
+            print("Running ADC")
+            self.mcp = mcp.mcp_external()
+
+            self.std_1 = 0
+            self.std_2 = 0
+
+            self.x1 = []
+            self.x2 = []"""
 
         # plot data: x, y values
         self.d = self.plot.plot(self.x, self.y)
@@ -25,6 +56,20 @@ class Plot2D(QtWidgets.QMainWindow):
         self.timer.setInterval(10) #50ms
         self.timer.timeout.connect(self.update_real_time)
         self.timer.start()
+
+    def runCapture(self):
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(10)  # 50ms
+        self.timer.timeout.connect(self.update_adc_measurement)
+        self.timer.start()
+
+    def update_adc_measurement(self):
+        x1 = float(self.mcp.read_IO(0)/65355*5)
+        x2 = float(self.mcp.read_IO(1) / 65355 * 5)
+        x1temp = self.x1[1:] if len(self.x1[1:]) <= 128 else self.x1[1:128]
+        x2temp = self.x2[1:] if len(self.x2[1:]) <= 128 else self.x2[1:128]
+        self.x1 = np.concatenate((x1temp, [x1]), axis=None)
+        self.x2 = np.concatenate((x2temp, [x2]), axis=None)
 
     def update_real_time(self):
         self.x=np.concatenate((self.x[1:], [self.x[-1]+0.01]), axis=None)
@@ -69,13 +114,17 @@ class Plot2D(QtWidgets.QMainWindow):
         if((sys.flags.interactive != 1) or not hasattr(QtCore, "PYQT_VERSION")):
             QtGui.QGuiApplication.instance().exec_()
 
+    def runADC(self):
+        pass
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main = Plot2D()
     main.setTitle("Dummy figure")
     main.setXAxis("time [h]", {'color':'r', 'font-size':'20px'})
     main.setYAxis("Temperature ")
-    main.yLimits([-10, 10], 2)
+    #main.yLimits([-10, 10], 2)
     #main.limits([0, 3.05, -10, 10])
     main.grid()
     main.show()
