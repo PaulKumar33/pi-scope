447c6c020e16d7ffd3feb86e84d5fd8801724cda
PaulKumar33 <paul.kumar@mail.utoronto.ca>
2021-06-09 22:29:32 -0400

    committing to develop a better plotter

--- qt_plotter.py
+++ qt_plotter.py
@@ -1,10 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets, QtCore
+from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-from PyQt5 import QtWidgets
-import sys
-from pyqtgraph.Qt import QtGui, QtCore
+import sys  # We need sys so that we can pass argv to QApplication
 import os
+from random import randint
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -14,11 +13,30 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0,100) for _ in range(100)]  # 100 data points
 
         self.graphWidget.setBackground('w')
-        self.graphWidget.plot(hour, temperature)
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+
+        
+        # ... init continued ...
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append( randint(0,100))  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
 
 
 def main():
@@ -30,6 +48,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
